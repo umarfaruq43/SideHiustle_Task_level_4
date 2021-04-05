36a4ca4a7ff4df6b68985f7cd5a3ae35960f316a
Umarfaruq43 <abdulazeezfaruq43@gmail.com>
2021-04-05 07:33:37 +0100

    updating the erroe message

--- formValidation.js
+++ formValidation.js
@@ -1,17 +1,40 @@
-getButton.addEventListener("click", (e) => {
-  if (getUsername.value == "" || getUsername.value == null) {
-    getSms[0].style.display = "block";
+// On Submit
+getForm.addEventListener("submit", (e) => {
+  if (getEmail.value !== user.email) {
+    err();
+    errSms.innerHTML = "User does not exist";
+
     e.preventDefault();
-  } else if (getemail.value == "" || getemail.value == null) {
-    getSms[1].style.display = "block";
-    e.preventDefault();
-  } else if (getPassword.value == "" || getPassword.value == null) {
-    getSms[2].style.display = "block";
-    e.preventDefault();
-  } else if (getPassword.value !== cPassword.value) {
-    getSms[3].style.display = "block";
+  } else if (getPassword.value != user.password) {
+    err();
+
     e.preventDefault();
   } else {
-    console.log(done);
+    alert("welcome on board");
   }
 });
+
+function err() {
+  getForm.addEventListener("submit", (e) => {
+    showErr();
+    setTimeout(() => {
+      existErr();
+    }, 2000);
+  });
+}
+
+// To close the error modall
+closeModal.addEventListener("click", (e) => {
+  getModal.style.opacity = 0;
+});
+
+function showErr() {
+  getModal.style.opacity = 100;
+  getModal.style.top = "10rem";
+  getLine.style.width = "100%";
+}
+function existErr() {
+  getModal.style.opacity = 0;
+  getModal.style.top = "3rem";
+  getLine.style.width = "0%";
+}
